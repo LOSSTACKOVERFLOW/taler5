eab69b25102c77cdba096a8ffb791814de506a33
edlealva <edlealva@espol.edu.ec>
2019-08-19 15:06:19 -0500

    Pilas2

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+class Libro(models.Model):
+    Titulo = models.CharField(max_length=200)
+    Isbn=models.CharField(max_length=12)
+    Calificación=models.IntegerField()
 
-# Create your models here.
+class Autor(models.Model):
+    Nombre = models.CharField(max_length=200)
+    Apellidos = models.CharField(max_length=200)
+
+
+class Usuario(models.Model):
+    Correo = models.CharField(max_length=200, primary_key=True)
+    Nombre = models.CharField(max_length=200)
+    Apellidos=models.CharField(max_length=200)
+    Cedula=models.CharField(max_length=10)
+    FechaNacimiento=models.DateField()
+
+class Usuario_Libro(models.Model):
+    Correo = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    IdLibro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+
+
+class Libro_Autor(models.Model):
+    IdAutor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    IdLibro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+
+
